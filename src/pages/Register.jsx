d66df0d87f23fbda7d64e89b45aacb64c4fcd360
sumeet pevekar <100433473+sumeetpevekar@users.noreply.github.com>
2025-03-27 21:11:26 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,9 +4,10 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../store/auth";
 import { toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-// const {storeTokenInLocale} = useAuth();
+
 
 const Register = () => {
+    const {storeTokenInLocale} = useAuth();
     const [user, setUser] = useState({
         name : "",
         username : "",
